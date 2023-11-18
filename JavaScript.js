184877d071251793add9e206ec855c1e19c09b15
EasyTools001 <150420149+EasyTools001@users.noreply.github.com>
2023-11-18 14:58:29 +0800

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,99 @@
+
+let title ; 
+let loadingFinished = false ;
+let time ;
+
+
+    
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "M_"
+
+    }, 300);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "Mo_"
+
+    }, 600);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "Mon_"
+
+    }, 900);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonM_"
+
+    }, 1300);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMa_"
+
+    }, 1900);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMan_"
+
+    }, 2300);
+    
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMang_"
+
+    }, 2600);
+
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonManga_"
+
+    }, 3000);
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMangaD_"
+
+    }, 3600);
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMangaDr_"
+
+    }, 3900);
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMangaDro_"
+
+    }, 4500);
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMangaDrop_"
+
+    }, 5000);
+    setTimeout(() => {
+        
+        title = document.getElementById("title")
+        title.innerHTML = "MonMangaDrop"
+        loadingFinished = true ;
+
+    }, 5400);
+
+    
+
+
+    console.warn("The loading has ended it took : " + "300ms" )
+    console.error("Do not let anyone write something here !")
